1fece713d6a644827c98716dc3b98cf42b87bb66
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-05-02 00:33:48 -0300

    primeiro commit/2

---
+++ src/main/java/com/matheusob25/workshop_spring_mongodb/WorkshopSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.matheusob25.workshop_spring_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopSpringMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matheusob25/workshop_spring_mongodb/WorkshopSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusob25.workshop_spring_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
