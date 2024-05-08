84f74b29359955f4624d90558b489e684e0ba1cf
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-05-08 13:51:43 -0300

    UserDTO

--- src/main/java/com/matheusob25/workshop_spring_mongodb/resources/UserResource.java
+++ src/main/java/com/matheusob25/workshop_spring_mongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.matheusob25.workshop_spring_mongodb.resources;
 
 import com.matheusob25.workshop_spring_mongodb.domain.User;
+import com.matheusob25.workshop_spring_mongodb.dto.UserDTO;
 import com.matheusob25.workshop_spring_mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> users = userService.findAll();
-        return ResponseEntity.ok().body(users);
+        List<UserDTO> userDTOs = users.stream().map(p -> new UserDTO(p)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOs);
     }
 }
